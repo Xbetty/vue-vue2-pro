e024525c23c0ff1e1632223994df2c81f50dadbf
xiongziting <xiongziting@100tal.com>
2019-04-09 10:48:26 +0800

    vue项目练习

--- src/components/ElementUI/index.vue
+++ src/components/ElementUI/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>Element-UI组件咯</h3>
+    <h3>Element-UI组件咯 好奇怪怎么没有这个项目啊</h3>
     <router-link :to="{path: '/eleIndex/input'}">input</router-link>
      <router-link :to="{path: '/eleIndex/upload'}">Upload</router-link>
      <router-link :to="{path: '/eleIndex/openAndCloseTextOne'}">文字过多展开与收起1</router-link>
